be87e841d78e13e2b3d18f763aba7a44adc68411
Dayn Augustson <daugustson@cnm.edu>
2018-11-18 15:50:31 -0700

    change text color and change font

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	initColorPicker();
+};
+
+function turnOn() {
+	var myCol = document.getElementById("P1");
+	myCol.style.color = "red";
+	myCol.style.fontFamily = "sans-serif";
+}
