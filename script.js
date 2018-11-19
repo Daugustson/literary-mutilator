7d81b61a4f20c6d8eb524293edd588cb07801a5c
Dayn Augustson <daugustson@cnm.edu>
2018-11-18 18:23:51 -0700

    trying to get the suffle to work

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ function hilight() {
 	s.addEventListener('keyup', find, false);
 
 }
+
+var list = document.getElementById("P3"),
+	button = document.getElementById("shuffle");
+function shuffle(items)
+{
+	var cached = items.slice(0), temp, i = cached.length, rand;
+	while(--i)
+	{
+		rand = Math.floor(i * Math.random());
+		temp = cached[rand];
+		cached[rand] = cached[i];
+		cached[i] = temp;
+	}
+	return cached;
+}
+function shuffleNodes()
+{
+	var nodes = list.children, i = 0;
+	nodes = Array.prototype.slice.call(nodes);
+	nodes = shuffle(nodes);
+	while(i < nodes.length)
+	{
+		list.appendChild(nodes[i]);
+		++i;
+	}
+}
+button.onclick = shuffleNodes;
